31d7149c4675c5ec2570ce55ed549e0ee38da3c9
rioma101 <rioma101@gmail.com>
2021-07-14 17:12:30 +0200

    nebitno

--- view/brodovi.js
+++ view/brodovi.js
@@ -317,7 +317,7 @@ function provjeri() {
     if(flag === 1 && counter === 20) { // ako imamo točno 20 brodova i svi su na dobrim mjestima, pobjeda
         $("#won").html('Čestitam, pobjeda!!');
         // ovdje ide dodavanje bodova useru
-        let score = 10000 / broj_provjera;
+        let score = round(10000 / broj_provjera);
         $.ajax({
             url: document.location.pathname + "?rt=igre/obradiRezultate",
             type: "POST",
